959ab04febf565047f826b4eb1a607dae8f3ec5c
abraham reta <95265657+areta1921@users.noreply.github.com>
2023-03-18 00:59:20 -0400

    first protfolio commit

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,26 @@
+import pro1 from "../assets/project1.png";
+import pro2 from "../assets/project2.png";
+import pro3 from "../assets/project3.png";
+
+const ProjectCardData = [
+  {
+    imgsrc: pro1,
+    title: "ECommerce Website Design",
+    text: "To put it simply, a paragraph should be as long or as short as necessary to develop a main idea. There is no absolute rule for how many sentences a paragraph must have",
+    view: "https://www.youtube.com/",
+  },
+  {
+    imgsrc: pro2,
+    title: "Course Website Design",
+    text: "To put it simply, a paragraph should be as long or as short as necessary to develop a main idea. There is no absolute rule for how many sentences a paragraph must have",
+    view: "https://www.youtube.com/",
+  },
+  {
+    imgsrc: pro3,
+    title: "Website Design",
+    text: "To put it simply, a paragraph should be as long or as short as necessary to develop a main idea. There is no absolute rule for how many sentences a paragraph must have",
+    view: "https://www.youtube.com/",
+  },
+];
+
+export default ProjectCardData;
